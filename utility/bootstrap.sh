fb5b7511bc2cbd2bb96c29d9db9c042650fb2af3
= <v.slavov96@gmail.com>
2017-06-06 12:31:56 +0300

    Adding management command and shell script

---
+++ utility/bootstrap.sh
@@ -0,0 +1,22 @@
+# Bootstrap script for local development. Does the following:
+# 1. Resets database
+# 2. Seeds with fakes
+
+E_USERNAME_NOT_CHANGED=60
+
+# Change this to your username
+
+USERNAME="v-slavov"
+DATABASE="odin"
+
+if [[ -z "$USERNAME" ]]
+then
+  echo "Open `basename $0` and change the USERNAME value"
+  exit "$E_USERNAME_NOT_CHANGED"
+fi
+
+dropdb --if-exists "$DATABASE"
+sudo -u postgres createdb -O $USERNAME "$DATABASE"
+
+python manage.py migrate
+python manage.py seed_initial
