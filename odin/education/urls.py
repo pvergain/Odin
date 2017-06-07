98daa629d210a157cf9e875f201b5a9cf9c0cd6d
= <v.slavov96@gmail.com>
2017-06-07 12:41:03 +0300

    Adding app_name to urls

--- odin/education/urls.py
+++ odin/education/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from .views import SampleProfileView
 
+app_name = 'education'
 
 urlpatterns = [
     url(r'^profile/$', SampleProfileView.as_view(), name='sample-profile'),
