5b767eabe75d07779fc9b39d46bda9eaf8ff48f2
= <v.slavov96@gmail.com>
2017-06-15 13:40:31 +0300

    Fixing profile page and social connections

--- odin/users/signals.py
+++ odin/users/signals.py
@@ -1,7 +1,9 @@
 from django.db.models.signals import post_save
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
+
 from allauth.account.signals import email_confirmed
+from allauth.socialaccount.signals import social_account_added
 
 from .models import BaseUser, Profile
 from .services import process_social_account
@@ -14,7 +16,14 @@ def create_profile_upon_user_creation(sender, instance, created, **kwargs):
 
 
 @receiver(email_confirmed)
-def social_accounts_process(request, *args, **kwargs):
+def social_accounts_process_on_confirmation(request, *args, **kwargs):
     user_instance = get_object_or_404(BaseUser, email=kwargs.get('email_address').email)
+    profile_instance = user_instance.profile
+    process_social_account(user_instance, profile_instance)
+
+
+@receiver(social_account_added)
+def social_accounts_process_on_add(request, sociallogin, **kwargs):
+    user_instance = sociallogin.user
     profile_instance = user_instance.profile
     process_social_account(user_instance, profile_instance)
