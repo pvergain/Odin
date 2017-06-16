4ede821b919e828ac6023bcc6936f38d0fe99df8
= <v.slavov96@gmail.com>
2017-06-16 11:40:24 +0300

    Add user management table

--- odin/dashboard/views.py
+++ odin/dashboard/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import TemplateView, RedirectView
+from django.views.generic import TemplateView, RedirectView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+
 from .mixins import ManagementDashboardPermissionMixin
+
+from odin.users.models import BaseUser
+from odin.education.models import Student, Teacher
 
 
 class RedirectToDashboardIndexView(RedirectView):
@@ -12,5 +16,14 @@ class DashboardIndexView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/index.html'
 
 
-class DashboardManagementView(ManagementDashboardPermissionMixin, TemplateView):
+class DashboardManagementView(ManagementDashboardPermissionMixin, ListView):
     template_name = 'dashboard/management.html'
+    paginate_by = 100
+
+    def get_queryset(self):
+        if self.request.GET.get('filter', None) == 'students':
+            return Student.objects.select_related('profile').all()
+        elif self.request.GET.get('filter', None) == 'teachers':
+            return Teacher.objects.select_related('profile').all()
+
+        return BaseUser.objects.select_related('profile').all()
