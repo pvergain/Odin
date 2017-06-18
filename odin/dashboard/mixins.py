dc1f9832d30161f482431f40c4908df885c28570
Radoslav Georgiev <radorado@hackbulgaria.com>
2017-06-18 14:03:26 +0300

    Add general comments and some style reordering for `dashboard` app

--- odin/dashboard/mixins.py
+++ odin/dashboard/mixins.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 
+"""
+COMMENT: This looks more like "permissions.py" then "mixins.py"
+Also - no need to add Mixin at the end of the class since it's not mixin
+"""
+
 
 class BaseUserPassesTestMixin(UserPassesTestMixin):
     def test_func(self):
@@ -8,9 +13,10 @@ class BaseUserPassesTestMixin(UserPassesTestMixin):
 
 class ManagementDashboardPermissionMixin(BaseUserPassesTestMixin):
     raise_exception = True
-    permission_denied_message = 'You must be a superuser to access this panel'
+    permission_denied_message = 'You must be a superuser to access this panel.'
 
     def test_func(self):
         if not self.request.user.is_superuser:
             return False
+
         return True and super().test_func()
