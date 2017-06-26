5939e89604d83d6c0e7132ab0825a1e5a0e344a4
Nimor111 <georgi.bojinov@hotmail.com>
2017-06-26 15:52:59 +0300

    One more test for service

--- odin/education/tests/test_services.py
+++ odin/education/tests/test_services.py
@@ -40,3 +40,18 @@ class TestCreateCourse(TestCase):
         with self.assertRaises(ValidationError):
             create_course(**data)
         self.assertEqual(1, Course.objects.count())
+
+    def test_create_course_creates_weeks_for_course_successfully(self):
+        self.assertEqual(0, Course.objects.count())
+        data = {
+            'name': faker.word(),
+            'start_date': parser.parse(faker.date()),
+            'end_date': parser.parse(faker.date()),
+            'repository': faker.url(),
+            'video_channel': faker.url(),
+            'facebook_group': faker.url(),
+        }
+        course = create_course(**data)
+        weeks = course.duration_in_weeks
+        self.assertEqual(1, Course.objects.count())
+        self.assertEqual(weeks, Week.objects.count())
