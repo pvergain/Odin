aac96a4c226ce10ed586ae8e22c281829cb73f95
Nimor111 <georgi.bojinov@hotmail.com>
2017-06-26 17:05:01 +0300

    Add test for service start day

--- odin/education/tests/test_services.py
+++ odin/education/tests/test_services.py
@@ -1,6 +1,7 @@
 from test_plus import TestCase
 
 from dateutil import parser
+from datetime import timedelta
 
 from django.core.exceptions import ValidationError
 
@@ -14,11 +15,12 @@ from odin.common.faker import faker
 class TestCreateCourse(TestCase):
 
     def test_course_is_created_successfully_with_valid_data(self):
+        start_date = parser.parse(faker.date())
         self.assertEqual(0, Course.objects.count())
         data = {
             'name': faker.word(),
-            'start_date': parser.parse(faker.date()),
-            'end_date': parser.parse(faker.date()),
+            'start_date': start_date,
+            'end_date': start_date + timedelta(days=faker.pyint()),
             'repository': faker.url(),
             'video_channel': faker.url(),
             'facebook_group': faker.url(),
@@ -27,12 +29,13 @@ class TestCreateCourse(TestCase):
         self.assertEqual(1, Course.objects.count())
 
     def test_create_course_raises_error_on_duplicate_name(self):
+        start_date = parser.parse(faker.date())
         course = CourseFactory()
         self.assertEqual(1, Course.objects.count())
         data = {
             'name': course.name,
-            'start_date': parser.parse(faker.date()),
-            'end_date': parser.parse(faker.date()),
+            'start_date': start_date,
+            'end_date': start_date + timedelta(days=faker.pyint()),
             'repository': faker.url(),
             'video_channel': faker.url(),
             'facebook_group': faker.url(),
@@ -42,11 +45,12 @@ class TestCreateCourse(TestCase):
         self.assertEqual(1, Course.objects.count())
 
     def test_create_course_creates_weeks_for_course_successfully(self):
+        start_date = parser.parse(faker.date())
         self.assertEqual(0, Course.objects.count())
         data = {
             'name': faker.word(),
-            'start_date': parser.parse(faker.date()),
-            'end_date': parser.parse(faker.date()),
+            'start_date': start_date,
+            'end_date': start_date + timedelta(days=faker.pyint()),
             'repository': faker.url(),
             'video_channel': faker.url(),
             'facebook_group': faker.url(),
@@ -55,3 +59,20 @@ class TestCreateCourse(TestCase):
         weeks = course.duration_in_weeks
         self.assertEqual(1, Course.objects.count())
         self.assertEqual(weeks, Week.objects.count())
+
+    def test_create_course_starts_week_from_monday(self):
+        start_date = parser.parse(faker.date())
+        data = {
+            'name': faker.word(),
+            'start_date': start_date,
+            'end_date': start_date + timedelta(days=faker.pyint()),
+            'repository': faker.url(),
+            'video_channel': faker.url(),
+            'facebook_group': faker.url(),
+        }
+        course = create_course(**data)
+        weeks = course.duration_in_weeks
+        self.assertEqual(1, Course.objects.count())
+        self.assertEqual(weeks, Week.objects.count())
+        week_one = Week.objects.first()
+        self.assertEqual(0, week_one.start_date.weekday())
