1de74df530a21e201e53780ebce2dda1c95c0b82
Vasil Slavov <v.slavov96@gmail.com>
2017-07-13 14:38:38 +0300

    Add validation for grading services + tests

--- odin/grading/services.py
+++ odin/grading/services.py
@@ -1,11 +1,15 @@
 from typing import Dict, BinaryIO
 
 from .models import GraderBinaryProblem, GraderPlainProblem
+<<<<<<< 01551c1857102ee984c6596c9db8c8247e218994
 <<<<<<< 23d2ac7b12094bdeef9042988b6f53a1e88164c4
 from .validators import create_problem_service_validation
 from .tasks import submit_solution
 =======
 >>>>>>> Add grader serializers and models
+=======
+from .validators import create_problem_service_validation
+>>>>>>> Add validation for grading services + tests
 
 
 def create_plain_problem(*,
@@ -15,7 +19,10 @@ def create_plain_problem(*,
                          code: str=None,
                          test: str=None,
                          extra_options: Dict={}) -> GraderPlainProblem:
+<<<<<<< 01551c1857102ee984c6596c9db8c8247e218994
 <<<<<<< 23d2ac7b12094bdeef9042988b6f53a1e88164c4
+=======
+>>>>>>> Add validation for grading services + tests
     if create_problem_service_validation(language=language,
                                          test_type=test_type,
                                          file_type=file_type):
@@ -27,6 +34,7 @@ def create_plain_problem(*,
             test=test,
             extra_options=extra_options
         )
+<<<<<<< 01551c1857102ee984c6596c9db8c8247e218994
 =======
     return GraderPlainProblem.objects.create(
         language=language,
@@ -37,6 +45,8 @@ def create_plain_problem(*,
         extra_options=extra_options
     )
 >>>>>>> Add grader serializers and models
+=======
+>>>>>>> Add validation for grading services + tests
 
 
 def create_binary_problem(*,
@@ -46,7 +56,10 @@ def create_binary_problem(*,
                           code: BinaryIO=None,
                           test: BinaryIO=None,
                           extra_options: Dict={}) -> GraderBinaryProblem:
+<<<<<<< 01551c1857102ee984c6596c9db8c8247e218994
 <<<<<<< 23d2ac7b12094bdeef9042988b6f53a1e88164c4
+=======
+>>>>>>> Add validation for grading services + tests
     if create_problem_service_validation(language=language,
                                          test_type=test_type,
                                          file_type=file_type):
@@ -58,6 +71,7 @@ def create_binary_problem(*,
             test=test,
             extra_options=extra_options
         )
+<<<<<<< 01551c1857102ee984c6596c9db8c8247e218994
 
 
 def start_grader_communication(solution_id):
@@ -72,3 +86,5 @@ def start_grader_communication(solution_id):
         extra_options=extra_options
     )
 >>>>>>> Add grader serializers and models
+=======
+>>>>>>> Add validation for grading services + tests
