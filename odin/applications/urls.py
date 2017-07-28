a531a79df31b9d9473a0d101bd39aa700200590c
Vasil Slavov <v.slavov96@gmail.com>
2017-07-28 14:41:19 +0300

    Add UpdateApplicationView

--- odin/applications/urls.py
+++ odin/applications/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 
-from .views import CreateApplicationInfoView
+from .views import CreateApplicationInfoView, EditApplicationView
 
 urlpatterns = [
     url(regex='^(?P<course_id>[0-9]+)/create-application-info/$',
         view=CreateApplicationInfoView.as_view(),
         name='create-application-info'),
+    url(regex='^(?P<course_id>[0-9]+)/edit-application-info/$',
+        view=EditApplicationView.as_view(),
+        name='edit-application-info'),
 ]
--- odin/applications/views.py
+++ odin/applications/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import FormView
+from django.views.generic import FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
@@ -6,36 +6,40 @@ from odin.education.mixins import CourseViewMixin, CallServiceMixin
 from odin.education.permissions import IsTeacherInCoursePermission
 from .forms import ApplicationInfoModelForm
 from .services import create_application_info
+from .mixins import ApplicationInfoFormDataMixin
 
 
 class CreateApplicationInfoView(CourseViewMixin,
                                 LoginRequiredMixin,
                                 IsTeacherInCoursePermission,
+                                ApplicationInfoFormDataMixin,
                                 CallServiceMixin,
                                 FormView):
     form_class = ApplicationInfoModelForm
     template_name = "applications/create_application_info.html"
 
-    def get_form_kwargs(self):
-        form_kwargs = super().get_form_kwargs()
-
-        if self.request.method in ('POST', 'PUT'):
-            post_data = self.request.POST
-            data = {}
-            data['start_date'] = post_data.get('start_date')
-            data['end_date'] = post_data.get('end_date')
-            data['start_interview_date'] = post_data.get('start_interview_date')
-            data['end_interview_date'] = post_data.get('end_interview_date')
-            data['description'] = post_data.get('description')
-            data['external_application_form'] = post_data.get('external_application_form')
-            data['course'] = self.course.id
-
-            form_kwargs['data'] = data
-        return form_kwargs
-
     def form_valid(self, form):
         self.call_service(service=create_application_info, service_kwargs=form.cleaned_data)
         return super().form_valid(form)
+
+    def get_success_url(self):
+        success_url = reverse_lazy('dashboard:education:user-course-detail',
+                                   kwargs={'course_id': self.course.id})
+
+        return success_url
+
+
+class EditApplicationView(CourseViewMixin,
+                          LoginRequiredMixin,
+                          IsTeacherInCoursePermission,
+                          ApplicationInfoFormDataMixin,
+                          UpdateView):
+
+    form_class = ApplicationInfoModelForm
+    template_name = "applications/create_application_info.html"
+
+    def get_object(self):
+        return self.course.application_info
 
     def get_success_url(self):
         success_url = reverse_lazy('dashboard:education:user-course-detail',
