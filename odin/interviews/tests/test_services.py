0bf21f27feb97c4d7feb9716e74844e01916e45f
Nimor111 <georgi.bojinov@hotmail.com>
2017-08-14 13:26:30 +0300

    Add tests for create new interview service

--- odin/interviews/tests/test_services.py
+++ odin/interviews/tests/test_services.py
@@ -6,6 +6,10 @@ from odin.education.factories import CourseFactory
 from odin.applications.factories import ApplicationInfoFactory
 from odin.interviews.services import add_course_to_interviewer_courses
 from odin.interviews.factories import InterviewerFactory
+from odin.applications.factories import ApplicationFactory
+
+from ..services import create_new_interview_for_application
+from ..factories import InterviewFactory
 
 
 class TestAddCourseToInterViewerCourses(TestCase):
@@ -24,3 +28,24 @@ class TestAddCourseToInterViewerCourses(TestCase):
         add_course_to_interviewer_courses(interviewer=self.interviewer, course=self.course)
 
         self.assertEqual(current_course_count + 1, self.interviewer.courses_to_interview.count())
+
+
+class TestCreateNewInterviewForApplication(TestCase):
+    def setUp(self):
+        self.application = ApplicationFactory()
+        self.interview = InterviewFactory(application=self.application)
+
+    def test_create_new_interview_raises_validation_error_when_chosen_interview_already_has_an_application(self):
+        self.new_interview = InterviewFactory(application=self.application)
+
+        with self.assertRaises(ValidationError):
+            create_new_interview_for_application(application=self.application,
+                                                 uuid=self.new_interview.uuid)
+
+    def test_create_new_interview_creates_new_interview_when_chosen_interview_is_free(self):
+        self.new_interview = InterviewFactory(application=None)
+        create_new_interview_for_application(application=self.application,
+                                             uuid=self.new_interview.uuid)
+
+        self.new_interview.refresh_from_db()
+        self.assertEqual(self.new_interview.application, self.application)
