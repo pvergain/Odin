2c34fb3cfa89d4cba2839443685df12f42600239
Radoslav Georgiev <radorado@hacksoft.io>
2017-09-14 17:38:06 +0300

    Factor-out recaptcha settings in `captcha.py`

--- config/settings/base.py
+++ config/settings/base.py
@@ -115,12 +115,7 @@ TEMPLATES = [
 ]
 
 from .allauth import *
-
-# Recaptcha settings
-NOCAPTCHA = True
-RECAPTCHA_PUBLIC_KEY = env('RECAPTCHA_PUBLIC_KEY', default='')
-RECAPTCHA_PRIVATE_KEY = env('RECAPTCHA_SECRET_KEY', default='')
-RECAPTCHA_USE_SSL = True
+from .captcha import *
 
 STATIC_ROOT = str(ROOT_DIR('staticfiles'))
 
