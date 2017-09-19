01414e252ba76eeed9ec1e2467b93ea6a94433d2
Radoslav Georgiev <radorado@hacksoft.io>
2017-09-19 11:13:10 +0300

    Improve AWS settings

--- config/settings/storages.py
+++ config/settings/storages.py
@@ -2,9 +2,9 @@ from django.conf import settings
 
 from storages.backends.s3boto3 import S3Boto3Storage
 
-# class MediaStorage(S3Boto3Storage):
-#     location = settings.MEDIA_LOCATION
-#     custom_domain = settings.MEDIA_S3_CUSTOM_DOMAIN
+class MediaStorage(S3Boto3Storage):
+    location = settings.MEDIA_LOCATION
+    custom_domain = settings.MEDIA_S3_CUSTOM_DOMAIN
 
 
 class StaticStorage(S3Boto3Storage):
