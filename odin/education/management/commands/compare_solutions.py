1924b91532c3c938535a3d4f17965f0e3839f0e0
slavov-v <v.slavov96@gmail.com>
2017-09-21 14:55:02 +0300

    Improve solution comparing command

--- odin/education/management/commands/compare_solutions.py
+++ odin/education/management/commands/compare_solutions.py
@@ -1,39 +1,56 @@
-import filecmp
+import difflib
+from pprint import pprint
 from collections import deque
 
 from django.core.management.base import BaseCommand, CommandError
 
 from odin.education.models import Course, Solution
 
+MIN_ALLOWED_DIFFERENCE_PERCENTAGE = 35
+
 
 def compare_code_solutions(first_solution, second_solution):
-    current_code, next_code = first_solution.code.strip('/n'), second_solution.code.strip('/n')
-    comparison = current_code == next_code
+    current_code = first_solution.code.splitlines()
+    next_code = second_solution.code.splitlines()
+    diff_percentage, unified_diff = calculate_difference_percentage(current_code, next_code)
 
-    if comparison:
+    if diff_percentage < MIN_ALLOWED_DIFFERENCE_PERCENTAGE:
         print(
             f"""
-             Matching contents in solutions
-             {first_solution} from {first_solution.student.email} and
-             {second_solution} from {second_solution.student.email}
-             on task {first_solution.task.name}
-             """
+            Matching contents in solutions
+            {first_solution} from {first_solution.student.email} and
+            {second_solution} from {second_solution.student.email}
+            on task {first_solution.task.name}
+            Differences:
+            """
         )
+        pprint(unified_diff)
 
 
 def compare_file_solutions(first_solution, second_solution):
-    current_filepath, next_filepath = first_solution.file.path, second_solution.file.path
-    comparison = filecmp.cmp(current_filepath, next_filepath, shallow=False)
+    current_code = first_solution.file.read().decode('utf-8').splitlines()
+    next_code = second_solution.file.read().decode('utf-8').splitlines()
+    diff_percentage, unified_diff = calculate_difference_percentage(current_code, next_code)
 
-    if comparison:
+    if diff_percentage < MIN_ALLOWED_DIFFERENCE_PERCENTAGE:
         print(
             f"""
-             Matching contents in files
-             {current_filepath} from {first_solution.student.email} and
-             {next_filepath} from {second_solution.student.email}
-             on Task: {first_solution.task.name}
-             """
+            Matching contents in files
+            {first_solution.file.name} from {first_solution.student.email} and
+            {second_solution.file.name} from {second_solution.student.email}
+            on Task: {first_solution.task.name}
+            Differences:
+            """
         )
+        pprint(unified_diff)
+
+
+def calculate_difference_percentage(first_chunk, second_chunk):
+    differences = list(difflib.unified_diff(first_chunk, second_chunk))[3:]
+    diff_count = 0
+    for difference in differences:
+        diff_count += difference.startswith('-') or difference.startswith('+')
+    return diff_count / len(first_chunk) * 100, differences
 
 
 class Command(BaseCommand):
@@ -58,6 +75,7 @@ class Command(BaseCommand):
                     passing_solutions = deque(
                         task.solutions.filter(status=Solution.OK).order_by('student__email').distinct('student__email')
                     )
+                    print(f'{len(passing_solutions)} people have solved {task.name}')
 
                     while passing_solutions:
                         current_solution = passing_solutions.popleft()
