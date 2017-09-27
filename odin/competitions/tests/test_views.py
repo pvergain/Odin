d15c4bb96965ee7994951efdcf5189c9183f672d
Nimor111 <georgi.bojinov@hotmail.com>
2017-09-27 11:41:47 +0300

    Finish tests for comp detail, add create competition tests

--- odin/competitions/tests/test_views.py
+++ odin/competitions/tests/test_views.py
@@ -1,11 +1,12 @@
 from test_plus import TestCase  # noqa
 
 from django.urls import reverse
+from django.utils import timezone
 
 from odin.common.faker import faker
-from odin.users.factories import BaseUserFactory
+from odin.users.factories import BaseUserFactory, SuperUserFactory
 
-from ..models import CompetitionMaterial, CompetitionJudge
+from ..models import CompetitionMaterial, CompetitionJudge, Competition
 from ..factories import (
     CompetitionFactory,
     CompetitionJudgeFactory,
@@ -38,6 +39,73 @@ class TestCompetitionDetailView(TestCase):
         with self.login(email=self.participant.email, password=self.test_password):
             response = self.get(self.url)
             self.assertEqual(200, response.status_code)
+
+    def test_raises_404_when_competition_does_not_exist(self):
+        url = reverse('competitions:competition-detail',
+                      kwargs={
+                          'competition_slug': self.competition.slug_url + str(faker.pyint())
+                      })
+        with self.login(email=self.user.email, password=self.test_password):
+            response = self.get(url_name=url)
+            self.response_404(response)
+
+
+class TestCreateCompetitionView(TestCase):
+    def setUp(self):
+        self.test_password = faker.password()
+        self.superuser = SuperUserFactory(password=self.test_password)
+        self.url = reverse('competitions:create-competition')
+
+    def test_access_to_url_forbidden_if_not_authenticated(self):
+        response = self.get(self.url)
+        self.response_403(response)
+
+    def test_access_to_url_forbidden_if_authenticated_and_not_superuser(self):
+        user = BaseUserFactory(password=self.test_password)
+
+        with self.login(email=user.email, password=self.test_password):
+            response = self.get(self.url)
+            self.response_403(response)
+
+    def test_access_to_url_allowed_if_superuser(self):
+        with self.login(email=self.superuser.email, password=self.test_password):
+            response = self.get(self.url)
+            self.response_200(response)
+
+    def test_does_not_create_competition_when_end_date_is_after_start_date(self):
+        competition_count = Competition.objects.count()
+        start_date = timezone.now()
+        end_date = start_date - timezone.timedelta(days=2)
+        data = {
+            'name': faker.name(),
+            'start_date': start_date.date(),
+            'end_date': end_date.date(),
+            'slug_url': faker.slug()
+        }
+
+        with self.login(email=self.superuser.email, password=self.test_password):
+            response = self.post(url_name=self.url, data=data)
+
+            self.response_200(response)
+            self.assertEqual(competition_count, Competition.objects.count())
+
+    def test_creates_competition_when_data_is_valid(self):
+        competition_count = Competition.objects.count()
+        start_date = timezone.now()
+        end_date = start_date + timezone.timedelta(days=2)
+
+        data = {
+            'name': faker.name(),
+            'start_date': start_date.date(),
+            'end_date': end_date.date(),
+            'slug_url': faker.slug()
+        }
+
+        with self.login(email=self.superuser.email, password=self.test_password):
+            response = self.post(url_name=self.url, data=data)
+
+            self.response_302(response)
+            self.assertEqual(competition_count + 1, Competition.objects.count())
 
 
 class TestAddNewCompetitionMaterialView(TestCase):
