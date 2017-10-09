a62c904a8f8b52057cd39b7982333180fb275416
Nimor111 <georgi.bojinov@hotmail.com>
2017-10-09 10:29:29 +0300

    Add competition to application info factory

--- odin/applications/factories.py
+++ odin/applications/factories.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from odin.common.faker import faker
 from odin.education.factories import CourseFactory
 from odin.users.factories import BaseUserFactory
+from odin.competitions.factories import CompetitionFactory
 from .models import (
     Application,
     ApplicationInfo,
@@ -19,6 +20,7 @@ class ApplicationInfoFactory(factory.DjangoModelFactory):
                                                  timezone.timedelta(days=faker.pyint()))
     end_interview_date = factory.LazyAttribute(lambda _: timezone.now().date() + timezone.timedelta(days=faker.pyint()))
     description = factory.LazyAttribute(lambda _: faker.text())
+    competition = factory.SubFactory(CompetitionFactory)
 
     class Meta:
         model = ApplicationInfo
