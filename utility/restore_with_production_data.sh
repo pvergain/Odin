e050410708d8e08d9d1697e8604a855d6f7a811f
Radoslav Georgiev <radorado@hacksoft.io>
2018-02-05 17:12:40 +0200

    Add utility script for testing with production data

---
+++ utility/restore_with_production_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USERNAME="radorado"
+
+echo "Fetching latest dump ...\n"
+curl -o db.dump `heroku pg:backups public-url --app odin-production`
+
+echo "Recreating database ...\n"
+dropdb --if-exists odin
+sudo -u postgres createdb -O $USERNAME odin
+
+echo "Starting pg_restore ...\n"
+pg_restore -U $USERNAME -d odin -c -j 4 db.dump
+rm db.dump
+
+echo "Migrations and data ...\n"
+python manage.py migrate
