477d68dc811f2008343058994336a39f94009b4c
Radoslav Georgiev <radorado@hacksoft.io>
2018-03-13 14:38:19 +0200

    Finalize `StudentCoursesApi` implementation
    - Add check for student
    - Filter courses for this student

--- odin/education/apis/courses.py
+++ odin/education/apis/courses.py
@@ -1,12 +1,24 @@
 from rest_framework import serializers
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import BasePermission
 
 from odin.authentication.apis.permissions import JSONWebTokenAuthenticationMixin
 
-from odin.education.models import Course
+from odin.education.models import Course, Student
 
 
 class StudentCoursesApi(JSONWebTokenAuthenticationMixin, ListAPIView):
+    class IsStudentPermission(BasePermission):
+        def has_permission(self, request, view):
+            user = request.user
+            student = user.downcast(Student)
+
+            if student is not None:
+                view.student = student
+                return True
+
+            return False
+
     class Serializer(serializers.ModelSerializer):
         class Meta:
             model = Course
@@ -19,5 +31,10 @@ class StudentCoursesApi(JSONWebTokenAuthenticationMixin, ListAPIView):
 
     serializer_class = Serializer
 
+    def get_permissions(self):
+        return super().get_permissions() + [self.IsStudentPermission()]
+
     def get_queryset(self):
-        return Course.objects.all()
+        return Course.objects\
+                     .filter(course_assignments__student=self.student)\
+                     .order_by('-id')
