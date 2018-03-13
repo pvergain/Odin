6ef0d64d0093fe97e3696b6d53440c34fb18f4b2
Radoslav Georgiev <radorado@hacksoft.io>
2018-03-13 13:48:21 +0200

    No need for default rest framework settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -92,17 +92,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    ),
-}
-
 JWT_AUTH = {
     'JWT_VERIFY': True,
     'JWT_VERIFY_EXPIRATION': False,
