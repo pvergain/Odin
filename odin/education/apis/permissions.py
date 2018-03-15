208aed027ed6c1379afd5b678ef2c7b693628da0
yaggul <dimitar.kotsev@hacksoft.io>
2018-03-15 16:33:08 +0200

    removing unneeded doc string

--- odin/education/apis/permissions.py
+++ odin/education/apis/permissions.py
@@ -6,13 +6,7 @@ from odin.education.models import Student
 
 
 class IsStudentPermission(BasePermission):
-    """
-    TODO: Write tests for that
 
-    1) Initialize IsStudentPermission
-    2) Call `has_permission` with `make_mock_object` for request, view
-    3) Assert correctness
-    """
     def has_permission(self, request, view):
         user = request.user
         student = user.downcast(Student)
