e6fa4a7eeb348dae2eaca87052f640ea426d0009
yaggul <dimitar.kotsev@hacksoft.io>
2018-03-16 16:28:25 +0200

    code clean up in login API

--- odin/education/apis/user.py
+++ odin/education/apis/user.py
@@ -1,15 +1,13 @@
-import datetime
-
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from rest_framework_jwt.views import ObtainJSONWebToken
 from rest_framework_jwt.settings import api_settings
 
 from .serializers import UserSerializer, ProfileSerializer
 
-from .permissions import StudentCourseAuthenticationMixin
+from .permissions import StudentCourseAuthenticationMixin, IsStudentPermission
 
 jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
 
@@ -18,25 +16,29 @@ class LoginUnitedApi(ObtainJSONWebToken):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
+        permission = IsStudentPermission()
 
         if serializer.is_valid():
+        
             user = serializer.object.get('user') or request.user
+            request.user = user
+
+            if not permission.has_permission(request, self):
+                self.permission_denied(
+                    request, message=getattr(permission, 'message', None)
+                )
+
             token = serializer.object.get('token')
+
             user_data = UserSerializer(instance=user).data
             profile_data = ProfileSerializer(instance=user.profile).data
             full_data = {**user_data, **profile_data}
+
             response_data = jwt_response_payload_handler(token, user, request)
             response_data.update({'me': full_data})
-            response = Response(response_data)
-            if api_settings.JWT_AUTH_COOKIE:
-                expiration = (datetime.utcnow() +
-                              api_settings.JWT_EXPIRATION_DELTA)
-                response.set_cookie(api_settings.JWT_AUTH_COOKIE,
-                                    token,
-                                    expires=expiration,
-                                    httponly=True)
-            return response
-
+        
+            return Response(response_data)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
