e86ec6c9fb8b4de3654185047e8117f55e73000b
yaggul <dimitar.kotsev@hacksoft.io>
2018-03-16 14:04:58 +0200

    code cleanup

--- odin/education/apis/serializers.py
+++ odin/education/apis/serializers.py
@@ -1,19 +1,6 @@
-'''
-stuent_id >> CourseAssignment >> course >> topic >> All included task >>Solution
-
-new solution << student + IncludedTask
-'''
 from rest_framework import serializers
 
-from odin.education.models import IncludedTask
-
 from odin.users.models import BaseUser, Profile
-
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = IncludedTask
-        fields = ('name', 'description', 'gradable')
 
 
 class ProfileSerializer(serializers.ModelSerializer):
