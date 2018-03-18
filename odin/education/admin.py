cfb997ae626ef20df202ab523f60ecee54f8abff
Radoslav Georgiev <radorado@hacksoft.io>
2018-03-18 13:03:13 +0200

    Add a bunch of useful education admins

--- odin/education/admin.py
+++ odin/education/admin.py
@@ -4,6 +4,9 @@ from .models import (
     ProgrammingLanguage,
     Course,
     Task,
+    IncludedTask,
+    Topic,
+    Week,
     Test,
     Solution
 )
@@ -22,6 +25,21 @@ class CourseAdmin(admin.ModelAdmin):
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('name', 'gradable')
+
+
+@admin.register(IncludedTask)
+class IncludedTaskAdmin(admin.ModelAdmin):
+    list_display = ('task', 'topic')
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'course', 'week')
+
+
+@admin.register(Week)
+class WeekAdmin(admin.ModelAdmin):
+    list_display = ('number', 'course', 'start_date', 'end_date')
 
 
 @admin.register(Test)
