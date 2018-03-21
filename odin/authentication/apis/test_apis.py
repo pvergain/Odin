8be5069d0cf0d39ecdee2846ad44050685a7f5a4
yaggul <dimitar.kotsev@hacksoft.io>
2018-03-21 11:57:36 +0200

    fixing flake8

--- odin/authentication/apis/test_apis.py
+++ odin/authentication/apis/test_apis.py
@@ -2,7 +2,7 @@ from test_plus import TestCase
 
 from odin.education.models import Student
 from odin.users.factories import BaseUserFactory
-from rest_framework_jwt.utils import jwt_decode_handler as decode
+# from rest_framework_jwt.utils import jwt_decode_handler as decode
 
 
 class TestUserSercetChange(TestCase):
@@ -17,7 +17,7 @@ class TestUserSercetChange(TestCase):
             user default user and login and get token
             create second user. login and get token
             1. try user1 to decode user 2 token
-            2. try user2 decode user1 token 
+            2. try user2 decode user1 token
             assert corectness
         '''
 
