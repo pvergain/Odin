ed7e7759def6c94d6da06087fa9fe61b7092f671
yaggul <dimitar.kotsev@hacksoft.io>
2018-03-22 17:07:41 +0200

    refactoring test_reset_password

--- odin/authentication/tests/services/test_reset_password.py
+++ odin/authentication/tests/services/test_reset_password.py
@@ -17,30 +17,27 @@ class ResetUserpasswordTests(TestCase):
         self.user.set_password(self.test_password)
         self.user.save()
         self.initial_secret_key = self.user.secret_key
+        self.password = faker.password()
 
     def test_user_cannot_reset_password_with_used_token(self):
-        password = faker.password()
         self.token.use()
 
         self.assertTrue(self.token.used)
         with self.assertRaises(ValidationError):
             reset_user_password(token=self.token,
-                                password=password)
+                                password=self.password)
 
     def test_user_cannot_reset_password_with_voided_token(self):
-        password = faker.password()
         self.token.void()
 
         self.assertTrue(self.token.voided)
         with self.assertRaises(ValidationError):
             reset_user_password(token=self.token,
-                                password=password)
+                                password=self.password)
 
     def test_user_can_reset_password_with_valid_token(self):
-        password = faker.password()
-
         user = reset_user_password(token=self.token,
-                                   password=password)
+                                   password=self.password)
 
         self.assertEqual(user, self.user)
         self.assertNotEqual(self.initial_secret_key, user.secret_key)
