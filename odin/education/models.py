0d613504180b468d8c099d6a68c82819389e8eb9
yaggul <dimitar.kotsev@hacksoft.io>
2018-03-26 12:46:14 +0300

    fixing indentations

--- odin/education/models.py
+++ odin/education/models.py
@@ -37,13 +37,17 @@ class Course(models.Model):
 
     attendable = models.BooleanField(default=True)
 
-    students = models.ManyToManyField(Student,
-                                      through='CourseAssignment',
-                                      through_fields=('course', 'student'))
+    students = models.ManyToManyField(
+        Student,
+        through='CourseAssignment',
+        through_fields=('course', 'student')
+    )
 
-    teachers = models.ManyToManyField(Teacher,
-                                      through='CourseAssignment',
-                                      through_fields=('course', 'teacher'))
+    teachers = models.ManyToManyField(
+        Teacher,
+        through='CourseAssignment',
+        through_fields=('course', 'teacher')
+    )
 
     slug_url = models.SlugField(unique=True)
 
@@ -153,18 +157,23 @@ class Material(BaseMaterial):
 
 
 class IncludedMaterial(BaseMaterial):
-    material = models.ForeignKey(Material,
-                                 on_delete=models.CASCADE,
-                                 related_name='included_materials')
-    week = models.ForeignKey('Week',
-                             related_name='materials',
-                             null=True,
-                             )
+    material = models.ForeignKey(
+        Material,
+        on_delete=models.CASCADE,
+        related_name='included_materials'
+    )
 
-    course = models.ForeignKey(Course,
-                               related_name='included_materials',
-                               null=True,
-                               )
+    week = models.ForeignKey(
+        'Week',
+        related_name='materials',
+        null=True,
+    )
+
+    course = models.ForeignKey(
+        Course,
+        related_name='included_materials',
+        null=True,
+    )
 
     class Meta:
         unique_together = (('week', 'material'), )
@@ -176,10 +185,12 @@ class Week(models.Model):
     end_date = models.DateField()
 
     number = models.PositiveIntegerField(default=1)
-    course = models.ForeignKey(Course,
-                               on_delete=models.CASCADE,
-                               related_name='weeks',
-                               null=True,)
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='weeks',
+        null=True,
+    )
 
     class Meta:
         ordering = ('number',)
@@ -259,19 +270,24 @@ class ProgrammingLanguage(models.Model):
 
 
 class IncludedTask(BaseTask):
-    task = models.ForeignKey(Task,
-                             on_delete=models.CASCADE,
-                             related_name='included_tasks')
-    week = models.ForeignKey(Week,
-                             on_delete=models.CASCADE,
-                             related_name='included_tasks',
-                             null=True,
-                             )
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+        related_name='included_tasks'
+    )
 
-    course = models.ForeignKey(Course,
-                               related_name='included_tasks',
-                               null=True,
-                               )
+    week = models.ForeignKey(
+        Week,
+        on_delete=models.CASCADE,
+        related_name='included_tasks',
+        null=True,
+    )
+
+    course = models.ForeignKey(
+        Course,
+        related_name='included_tasks',
+        null=True,
+    )
 
     objects = TaskQuerySet.as_manager()
 
