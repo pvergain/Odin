96dada6436a7231d5f5f35acbb9af370f9e00e77
yaggul <dimitar.kotsev@hacksoft.io>
2018-04-26 18:15:41 +0300

    Adding test for IsTeacherPermission

--- odin/education/apis/tests/test_permissions.py
+++ odin/education/apis/tests/test_permissions.py
@@ -1,7 +1,14 @@
 from test_plus import TestCase
-from odin.education.apis.permissions import IsStudentPermission
+from odin.education.apis.permissions import (
+    IsStudentPermission,
+    IsTeacherPermission,
+    IsStudentOrTeacherInCoursePermission,
+    IsStudentOrTeacherPermission,
+)
+
 from odin.users.factories import BaseUserFactory
-from odin.education.models import Student
+from odin.education.models import Student, Teacher
+from unittest.mock import Mock
 
 
 def make_mock_object(**kwargs):
@@ -25,3 +32,57 @@ class TestIsStudenPermission(TestCase):
         permissions = IsStudentPermission()
 
         self.assertFalse(permissions.has_permission(self.request, None))
+
+
+class TestIsTeacherPermission(TestCase):
+    def setUp(self):
+        self.user = BaseUserFactory()
+        self.request = Mock()
+        self.request.user = self.user
+
+    def test_permission_if_user_is_not_teacher(self):
+        permission = IsTeacherPermission()
+
+        self.assertFalse(permission.has_permission(self.request, None))
+
+    def test_permission_if_user_is_teacher(self):
+        permission = IsTeacherPermission()
+
+        teacher = Teacher.objects.create_from_user(self.user)
+        teacher.save()
+
+        self.assertTrue(permission.has_permission(self.request, None))
+
+
+class TestIsStudentOrTeacherPermission(TestCase):
+    def setUp(self):
+        pass
+
+    def test_permission_if_user_is_not_student_or_teacher(self):
+        pass
+
+    def test_permission_if_user_is_student(self):
+        pass
+
+    def test_permission_if_user_is_teacher(self):
+        pass
+
+    def test_permission_if_user_is_teacher_and_student(self):
+        pass
+
+
+class IsStudentOrTeacherInCoursePermission(TestCase):
+    def setUp(self):
+        pass
+
+    def test_permission_if_user_is_not_student_or_teacher_in_course(self):
+        pass
+
+    def test_permission_if_user_is_student_in_course(self):
+        pass
+
+    def test_permission_if_user_is_teacher_in_course(self):
+        pass
+
+    def test_permission_if_user_is_student_and_teacher_in_course(self):
+        pass
