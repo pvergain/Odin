42560e167fac5a90c7c05d74b531a745643d887d
yaggul <dimitar.kotsev@hacksoft.io>
2018-04-27 11:51:55 +0300

    Adding test for all odin/education/apis/permissions

--- odin/education/apis/tests/test_permissions.py
+++ odin/education/apis/tests/test_permissions.py
@@ -7,7 +7,8 @@ from odin.education.apis.permissions import (
 )
 
 from odin.users.factories import BaseUserFactory
-from odin.education.models import Student, Teacher
+from odin.education.models import Student, Teacher, CourseAssignment
+from odin.education.factories import CourseFactory
 from unittest.mock import Mock
 
 
@@ -26,11 +27,13 @@ class TestIsStudenPermission(TestCase):
 
         permissions = IsStudentPermission()
 
+        self.assertFalse(self.user.is_teacher())
         self.assertTrue(permissions.has_permission(self.request, None))
 
     def test_permission_if_user_is_not_student(self):
         permissions = IsStudentPermission()
 
+        self.assertFalse(self.user.is_student())
         self.assertFalse(permissions.has_permission(self.request, None))
 
 
@@ -43,6 +46,7 @@ class TestIsTeacherPermission(TestCase):
     def test_permission_if_user_is_not_teacher(self):
         permission = IsTeacherPermission()
 
+        self.assertFalse(self.user.is_teacher())
         self.assertFalse(permission.has_permission(self.request, None))
 
     def test_permission_if_user_is_teacher(self):
@@ -56,33 +60,120 @@ class TestIsTeacherPermission(TestCase):
 
 class TestIsStudentOrTeacherPermission(TestCase):
     def setUp(self):
-        pass
+        self.user = BaseUserFactory()
+        self.user.is_active = True
+        self.user.save()
+        self.request = Mock()
+        self.request.user = self.user
 
     def test_permission_if_user_is_not_student_or_teacher(self):
-        pass
+        permissions = IsStudentOrTeacherPermission()
+
+        self.assertFalse(self.user.is_student())
+        self.assertFalse(self.user.is_teacher())
+        self.assertFalse(permissions.has_permission(self.request, None))
 
     def test_permission_if_user_is_student(self):
-        pass
+        student = Student.objects.create_from_user(self.user)
+        student.save()
+
+        permissions = IsStudentOrTeacherPermission()
+
+        self.assertTrue(self.user.is_student())
+        self.assertFalse(self.user.is_teacher())
+        self.assertTrue(permissions.has_permission(self.request, None))
 
     def test_permission_if_user_is_teacher(self):
-        pass
+        teacher = Teacher.objects.create_from_user(self.user)
+        teacher.save()
+
+        permissions = IsStudentOrTeacherPermission()
+
+        self.assertTrue(self.user.is_teacher())
+        self.assertFalse(self.user.is_student())
+        self.assertTrue(permissions.has_permission(self.request, None))
 
     def test_permission_if_user_is_teacher_and_student(self):
-        pass
+        teacher = Teacher.objects.create_from_user(self.user)
+        teacher.save()
+
+        student = Student.objects.create_from_user(self.user)
+        student.save()
+
+        permissions = IsStudentOrTeacherPermission()
+
+        self.assertTrue(self.user.is_student())
+        self.assertTrue(self.user.is_teacher())
+        self.assertTrue(permissions.has_permission(self.request, None))
 
 
-class IsStudentOrTeacherInCoursePermission(TestCase):
+class TestIsStudentOrTeacherInCoursePermission(TestCase):
     def setUp(self):
-        pass
+        self.user = BaseUserFactory()
+        self.user.is_active = True
+        self.user.save()
+        self.request = Mock()
+        self.request.user = self.user
+        self.course = CourseFactory()
+        self.view = make_mock_object()
+        self.view.kwargs = {'course_id': self.course.id}
 
     def test_permission_if_user_is_not_student_or_teacher_in_course(self):
-        pass
+
+        permissions = IsStudentOrTeacherInCoursePermission()
+
+        self.assertFalse(permissions.has_permission(self.request, self.view))
 
     def test_permission_if_user_is_student_in_course(self):
-        pass
+        student = Student.objects.create_from_user(self.user)
+        student.save()
+
+        ca = CourseAssignment()
+
+        ca.course = self.course
+        ca.student = student
+        ca.save()
+
+        permissions = IsStudentOrTeacherInCoursePermission()
+
+        self.assertTrue(self.user.is_student())
+        self.assertTrue(permissions.has_permission(self.request, self.view))
 
     def test_permission_if_user_is_teacher_in_course(self):
-        pass
+        teacher = Teacher.objects.create_from_user(self.user)
+        teacher.save()
+
+        ca = CourseAssignment()
+
+        ca.course = self.course
+        ca.teacher = teacher
+        ca.save()
+
+        permissions = IsStudentOrTeacherInCoursePermission()
+
+        self.assertTrue(self.user.is_teacher())
+        self.assertTrue(permissions.has_permission(self.request, self.view))
 
     def test_permission_if_user_is_student_and_teacher_in_course(self):
-        pass
+        teacher = Teacher.objects.create_from_user(self.user)
+        teacher.save()
+
+        student = Student.objects.create_from_user(self.user)
+        student.save()
+
+        ca1 = CourseAssignment()
+        ca2 = CourseAssignment()
+
+        ca1.course = self.course
+        ca1.teacher = teacher
+        ca1.save()
+
+        ca2.course = self.course
+        ca2.student = student
+        ca2.save()
+
+        permissions = IsStudentOrTeacherInCoursePermission()
+
+        self.assertTrue(self.user.is_teacher())
+        self.assertTrue(self.user.is_student())
+        self.assertTrue(permissions.has_permission(self.request, self.view))
