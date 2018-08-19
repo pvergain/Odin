1569a27429867bf05f3d144a1f079f3a8115dc0a
Radoslav Georgiev <radorado@hacksoft.io>
2018-08-19 16:04:22 +0300

    Split old `CourseTests`

--- odin/education/tests/models/test_course.py
+++ odin/education/tests/models/test_course.py
@@ -22,3 +22,111 @@ class CourseTests(TestCase):
 
         with self.assertRaises(ValidationError):
             course.full_clean()
+
+    def test_course_has_started_if_start_date_is_in_the_past(self):
+        start_date = (get_now() - timedelta(days=1)).date()
+        end_date = (get_now() + timedelta(days=1)).date()
+
+        course_data = CourseFactory.build()
+        course_data['start_date'] = start_date
+        course_data['end_date'] = end_date
+
+        course = Course(**course_data)
+
+        self.assertTrue(course.has_started)
+
+    def test_course_has_started_if_start_date_is_today(self):
+        start_date = get_now().date()
+        end_date = (get_now() + timedelta(days=1)).date()
+
+        course_data = CourseFactory.build()
+        course_data['start_date'] = start_date
+        course_data['end_date'] = end_date
+
+        course = Course(**course_data)
+
+        self.assertTrue(course.has_started)
+
+    def test_course_has_not_started_if_start_date_is_in_the_future(self):
+        start_date = (get_now() + timedelta(days=1)).date()
+        end_date = (get_now() + timedelta(days=2)).date()
+
+        course_data = CourseFactory.build()
+        course_data['start_date'] = start_date
+        course_data['end_date'] = end_date
+
+        course = Course(**course_data)
+
+        self.assertFalse(course.has_started)
+
+    def test_course_has_finished_with_end_date_today(self):
+        start_date = (get_now() - timedelta(days=1)).date()
+        end_date = get_now().date()
+
+        course_data = CourseFactory.build()
+        course_data['start_date'] = start_date
+        course_data['end_date'] = end_date
+
+        course = Course(**course_data)
+
+        self.assertTrue(course.has_finished)
+
+    def test_course_has_finished_with_end_date_bigger_than_today(self):
+        start_date = (get_now() + timedelta(days=1)).date()
+        end_date = (get_now() + timedelta(days=2)).date()
+
+        course_data = CourseFactory.build()
+        course_data['start_date'] = start_date
+        course_data['end_date'] = end_date
+
+        course = Course(**course_data)
+
+        self.assertFalse(course.has_finished)
+
+    def test_can_generate_certificates_with_end_date_in_time_gap(self):
+        start_date = (get_now() + timedelta(days=1)).date()
+        end_date = (get_now() + timedelta(days=2)).date()
+
+        course_data = CourseFactory.build()
+        course_data['start_date'] = start_date
+        course_data['end_date'] = end_date
+
+        course = Course(**course_data)
+
+        self.assertTrue(course.can_generate_certificates)
+
+    def test_can_generate_certificates_with_end_date_outside_time_gap(self):
+        start_date = (get_now() - timedelta(days=30)).date()
+        end_date = (get_now() - timedelta(days=16)).date()
+
+        course_data = CourseFactory.build()
+        course_data['start_date'] = start_date
+        course_data['end_date'] = end_date
+
+        course = Course(**course_data)
+
+        self.assertFalse(course.can_generate_certificates)
+
+    def test_duration_in_weeks_should_return_two(self):
+        start_date = get_now().date()
+        end_date = (get_now() + timedelta(days=13)).date()
+
+        course_data = CourseFactory.build()
+        course_data['start_date'] = start_date
+        course_data['end_date'] = end_date
+
+        course = Course(**course_data)
+
+        self.assertEqual(2, course.duration_in_weeks)
+
+    def test_duration_in_weeks_should_return_three(self):
+        start_date = get_now().date()
+        end_date = (get_now() + timedelta(days=14)).date()
+
+        course_data = CourseFactory.build()
+        course_data['start_date'] = start_date
+        course_data['end_date'] = end_date
+
+        course = Course(**course_data)
+
+        self.assertEqual(3, course.duration_in_weeks)
--- odin/education/tests/test_models_and_managers.py
+++ odin/education/tests/test_models_and_managers.py
@@ -1,11 +1,8 @@
-from datetime import timedelta
-
 from test_plus import TestCase
 
 from django.core.exceptions import ValidationError
 
 from odin.common.faker import faker
-from odin.common.utils import get_now
 
 from odin.users.models import BaseUser
 from odin.users.factories import BaseUserFactory
@@ -19,7 +16,7 @@ from ..factories import (
     WeekFactory
 )
 from ..models import Student, Teacher, CourseAssignment, IncludedTask, Solution
-from ..services import add_student, add_teacher
+from ..services import add_student
 
 
 class StudentTests(TestCase):
@@ -180,144 +177,6 @@ class CourseAssignmentTests(TestCase):
 
         with self.assertRaises(ValidationError):
             CourseAssignment.objects.create(course=course)
-
-
-class CourseTests(TestCase):
-    def test_teachers_property_works(self):
-        course = CourseFactory()
-        teacher = TeacherFactory()
-
-        count = course.visible_teachers.count()
-
-        CourseAssignment.objects.create(course=course,
-                                        teacher=teacher)
-
-        self.assertEqual(count + 1, course.teachers.count())
-        self.assertEqual(teacher, course.teachers.first().downcast(Teacher))
-
-    def test_students_property_works(self):
-        course = CourseFactory()
-        student = StudentFactory()
-
-        count = course.students.count()
-
-        CourseAssignment.objects.create(course=course,
-                                        student=student)
-
-        self.assertEqual(count + 1, course.students.count())
-        self.assertEqual(student, course.students.first().downcast(Student))
-
-    def test_students_and_teachers_properties_work_for_user_that_is_both(self):
-        course = CourseFactory()
-
-        student = StudentFactory()
-        teacher = Teacher.objects.create_from_user(student.user)
-
-        student_count = course.students.count()
-        teacher_count = course.teachers.count()
-
-        CourseAssignment.objects.create(course=course,
-                                        student=student)
-
-        CourseAssignment.objects.create(course=course,
-                                        teacher=teacher)
-
-        self.assertEqual(student_count + 1, course.students.count())
-        self.assertEqual(teacher_count + 1, course.teachers.count())
-
-        self.assertEqual(student, course.students.first().downcast(Student))
-        self.assertEqual(teacher, course.teachers.first().downcast(Teacher))
-
-    def test_course_has_started_if_start_date_is_in_the_past(self):
-        start_date = (get_now() - timedelta(days=1)).date()
-        end_date = (get_now() + timedelta(days=1)).date()
-
-        course = CourseFactory(start_date=start_date,
-                               end_date=end_date)
-
-        self.assertTrue(course.has_started)
-
-    def test_course_has_started_if_start_date_is_today(self):
-        start_date = get_now().date()
-        end_date = (get_now() + timedelta(days=1)).date()
-
-        course = CourseFactory(start_date=start_date,
-                               end_date=end_date)
-
-        self.assertTrue(course.has_started)
-
-    def test_course_has_not_started_if_start_date_is_in_the_future(self):
-        start_date = (get_now() + timedelta(days=1)).date()
-        end_date = (get_now() + timedelta(days=2)).date()
-
-        course = CourseFactory(start_date=start_date,
-                               end_date=end_date)
-
-        self.assertFalse(course.has_started)
-
-    def test_create_course_with_multiple_students(self):
-        students = StudentFactory.create_batch(3)
-        course = CourseFactory()
-
-        count = course.students.count()
-
-        for student in students:
-            add_student(course, student)
-
-        self.assertEqual(count + 3, course.students.count())
-
-    def test_create_course_with_multiple_teachers(self):
-        teachers = TeacherFactory.create_batch(3)
-        course = CourseFactory()
-
-        count = course.teachers.count()
-
-        for teacher in teachers:
-            add_teacher(course, teacher)
-
-        self.assertEqual(count + 3, course.teachers.count())
-
-    def test_course_has_finished_with_end_date_today(self):
-        start_date = (get_now() - timedelta(days=1)).date()
-        end_date = get_now().date()
-        course = CourseFactory(start_date=start_date, end_date=end_date)
-
-        self.assertTrue(course.has_finished)
-
-    def test_course_has_finished_with_end_date_bigger_than_today(self):
-        start_date = (get_now() + timedelta(days=1)).date()
-        end_date = (get_now() + timedelta(days=2)).date()
-        course = CourseFactory(start_date=start_date, end_date=end_date)
-
-        self.assertFalse(course.has_finished)
-
-    def test_can_generate_certificates_with_end_date_in_time_gap(self):
-        start_date = (get_now() + timedelta(days=1)).date()
-        end_date = (get_now() + timedelta(days=2)).date()
-        course = CourseFactory(start_date=start_date, end_date=end_date)
-
-        self.assertTrue(course.can_generate_certificates)
-
-    def test_can_generate_certificates_with_end_date_outside_time_gap(self):
-        start_date = (get_now() - timedelta(days=30)).date()
-        end_date = (get_now() - timedelta(days=16)).date()
-        course = CourseFactory(start_date=start_date, end_date=end_date)
-
-        self.assertFalse(course.can_generate_certificates)
-
-    def test_duration_in_weeks_should_return_two(self):
-        start_date = get_now().date()
-        end_date = (get_now() + timedelta(days=13)).date()
-        course = CourseFactory(start_date=start_date, end_date=end_date)
-
-        self.assertEqual(2, course.duration_in_weeks)
-
-    def test_duration_in_weeks_should_return_three(self):
-        start_date = get_now().date()
-        end_date = (get_now() + timedelta(days=14)).date()
-        course2 = CourseFactory(start_date=start_date, end_date=end_date)
-
-        self.assertEqual(3, course2.duration_in_weeks)
 
 
 class TaskTests(TestCase):
